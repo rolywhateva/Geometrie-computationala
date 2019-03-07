cc04c74bfdefc888e21caece8df2f7aa16523426
veres roland <rolandveres@yahoo.com>
2019-03-07 09:56:00 +0200

    Puncte coliniare

--- Laborator1/Laborator1/Form1.Designer.cs
+++ Laborator1/Laborator1/Form1.Designer.cs
@@ -28,20 +28,205 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxXA = new System.Windows.Forms.TextBox();
+            this.textBoxYA = new System.Windows.Forms.TextBox();
+            this.textBoxXB = new System.Windows.Forms.TextBox();
+            this.textBoxYB = new System.Windows.Forms.TextBox();
+            this.textBoxXC = new System.Windows.Forms.TextBox();
+            this.textBoxYc = new System.Windows.Forms.TextBox();
+            this.ButonExecuta = new System.Windows.Forms.Button();
+            this.labelColiniare = new System.Windows.Forms.Label();
+            this.labelTriunghi = new System.Windows.Forms.Label();
+            this.box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(565, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(565, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ya";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Xb";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(565, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Yb";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(574, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Xc";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(574, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Yc";
+            // 
+            // textBoxXA
+            // 
+            this.textBoxXA.Location = new System.Drawing.Point(614, 47);
+            this.textBoxXA.Name = "textBoxXA";
+            this.textBoxXA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXA.TabIndex = 6;
+            // 
+            // textBoxYA
+            // 
+            this.textBoxYA.Location = new System.Drawing.Point(614, 90);
+            this.textBoxYA.Name = "textBoxYA";
+            this.textBoxYA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYA.TabIndex = 7;
+            // 
+            // textBoxXB
+            // 
+            this.textBoxXB.Location = new System.Drawing.Point(614, 134);
+            this.textBoxXB.Name = "textBoxXB";
+            this.textBoxXB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXB.TabIndex = 8;
+            // 
+            // textBoxYB
+            // 
+            this.textBoxYB.Location = new System.Drawing.Point(614, 175);
+            this.textBoxYB.Name = "textBoxYB";
+            this.textBoxYB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYB.TabIndex = 9;
+            // 
+            // textBoxXC
+            // 
+            this.textBoxXC.Location = new System.Drawing.Point(614, 222);
+            this.textBoxXC.Name = "textBoxXC";
+            this.textBoxXC.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXC.TabIndex = 10;
+            // 
+            // textBoxYc
+            // 
+            this.textBoxYc.Location = new System.Drawing.Point(614, 262);
+            this.textBoxYc.Name = "textBoxYc";
+            this.textBoxYc.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYc.TabIndex = 11;
+            // 
+            // ButonExecuta
+            // 
+            this.ButonExecuta.Location = new System.Drawing.Point(610, 360);
+            this.ButonExecuta.Name = "ButonExecuta";
+            this.ButonExecuta.Size = new System.Drawing.Size(75, 23);
+            this.ButonExecuta.TabIndex = 12;
+            this.ButonExecuta.Text = "Apasa";
+            this.ButonExecuta.UseVisualStyleBackColor = true;
+            this.ButonExecuta.Click += new System.EventHandler(this.ButonExecuta_Click);
+            // 
+            // labelColiniare
+            // 
+            this.labelColiniare.AutoSize = true;
+            this.labelColiniare.Location = new System.Drawing.Point(574, 312);
+            this.labelColiniare.Name = "labelColiniare";
+            this.labelColiniare.Size = new System.Drawing.Size(47, 13);
+            this.labelColiniare.TabIndex = 13;
+            this.labelColiniare.Text = "Coliniare";
+            // 
+            // labelTriunghi
+            // 
+            this.labelTriunghi.AutoSize = true;
+            this.labelTriunghi.Location = new System.Drawing.Point(574, 335);
+            this.labelTriunghi.Name = "labelTriunghi";
+            this.labelTriunghi.Size = new System.Drawing.Size(111, 13);
+            this.labelTriunghi.TabIndex = 14;
+            this.labelTriunghi.Text = "Isoscel sau echilateral";
+            // 
+            // box
+            // 
+            this.box.Location = new System.Drawing.Point(12, 12);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(547, 582);
+            this.box.TabIndex = 15;
+            this.box.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(985, 606);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.labelTriunghi);
+            this.Controls.Add(this.labelColiniare);
+            this.Controls.Add(this.ButonExecuta);
+            this.Controls.Add(this.textBoxYc);
+            this.Controls.Add(this.textBoxXC);
+            this.Controls.Add(this.textBoxYB);
+            this.Controls.Add(this.textBoxXB);
+            this.Controls.Add(this.textBoxYA);
+            this.Controls.Add(this.textBoxXA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Laborator1 ";
+            this.Text = "Pucte coliniare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxXA;
+        private System.Windows.Forms.TextBox textBoxYA;
+        private System.Windows.Forms.TextBox textBoxXB;
+        private System.Windows.Forms.TextBox textBoxYB;
+        private System.Windows.Forms.TextBox textBoxXC;
+        private System.Windows.Forms.TextBox textBoxYc;
+        private System.Windows.Forms.Button ButonExecuta;
+        private System.Windows.Forms.Label labelColiniare;
+        private System.Windows.Forms.Label labelTriunghi;
+        private System.Windows.Forms.PictureBox box;
     }
 }
 
--- Laborator1/Laborator1/Form1.cs
+++ Laborator1/Laborator1/Form1.cs
@@ -16,5 +16,109 @@ namespace Laborator1
         {
             InitializeComponent();
         }
+        Graphics grp;
+        Bitmap map;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelTriunghi.Visible = false;
+            labelColiniare.Visible = false;
+            map = new Bitmap(box.Width, box.Height);
+            grp = Graphics.FromImage(map);
+          //  grp.DrawLine(new Pen(Color.Pink), 30, 40, 100, 200);
+           // box.Image = map;
+
+        }
+        private bool coliniare(PointF A, PointF B,PointF C)
+        {
+            float det = A.X * B.Y + B.X * C.Y + C.X * A.Y - 
+                        (C.X * B.Y + A.X * C.Y + B.X * A.Y);
+            return det == 0;
+        }
+        private  float  distanta(PointF A,PointF B)
+        {
+            return (float)Math.Sqrt((B.X - A.X) * (B.X - A.X) + (B.Y - A.Y) * (B.Y - A.Y));
+        }
+        private void DrawPoint(Pen pen,  PointF p,int width)
+        {
+            grp.DrawEllipse(pen, p.X,p.Y, width, width);
+            SolidBrush sb = new SolidBrush(pen.Color);
+            grp.FillEllipse(sb, p.X, p.Y, width, width);
+            box.Image = map;
+        }
+        private  void DrawTriunghi(Pen pen,int width ,params PointF[] p)
+        {
+            grp.DrawPolygon(pen, p);
+            SolidBrush sb = new SolidBrush(pen.Color);
+            grp.FillPolygon(sb, p);
+            box.Image = map;
+        }
+        private void ButonExecuta_Click(object sender, EventArgs e)
+        {
+            PointF A = new PointF();
+            PointF B = new PointF();
+            PointF C = new PointF();
+            A.X = float.Parse(textBoxXA.Text);
+            A.Y = float.Parse(textBoxYA.Text);
+            B.X = float.Parse(textBoxXB.Text);
+            B.Y = float.Parse(textBoxYB.Text);
+            C.X = float.Parse(textBoxXC.Text);
+            C.Y = float.Parse(textBoxYc.Text);
+            bool col = coliniare(A, B, C);
+            labelColiniare.Visible = true;
+            Pen p = new Pen(Color.Pink);
+            int width = 6;
+            grp.Clear(box.BackColor);
+            DrawPoint(p, A, width);
+            DrawPoint(p, B, width);
+            DrawPoint(p, C, width);
+
+
+
+
+
+            if (col)
+            {
+
+                labelColiniare.Text = string.Format("Punctele {0},{1},{2} sunt coliniare", A, B, C);
+            }else
+            {
+                p.Color = Color.Red;
+
+                DrawTriunghi(p, width, A,B,C);
+                labelColiniare.Text = string.Format("Punctele {0},{1},{2}nu sunt coliniare", A, B, C);
+
+                float AB = distanta(A, B);
+                float BC = distanta(B, C);
+                float AC = distanta(A, C);
+                if (AB == BC && BC == AC)
+                {
+                    labelTriunghi.Visible = true;
+                    labelTriunghi.Text = "Triunghiul este echilateral";
+                }
+                else
+                {
+
+                    labelTriunghi.Visible = true;
+                    if (AB == BC || BC == AC || AB == AC)
+
+                        labelTriunghi.Text = string.Format("Triunghiul format din punctele {0},{1},{2} este  isoscel", A, B, C);
+                    else
+
+                        labelTriunghi.Text = string.Format("Triunghiul format din punctele {0},{1},{2}  nu este  isoscel", A, B, C);
+
+
+                }
+
+
+
+
+
+
+            }
+
+
+
+
+        }
     }
 }
