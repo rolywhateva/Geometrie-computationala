ee4103d4cd9a601a0498f9b743b43041908a3c77
veres roland <rolandveres@yahoo.com>
2019-03-14 09:50:02 +0200

    Triunghiuri

--- Laborator2/Laborator2/Form1.Designer.cs
+++ Laborator2/Laborator2/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.Box = new System.Windows.Forms.PictureBox();
+            this.BTriunghi = new System.Windows.Forms.Button();
+            this.BPatrate = new System.Windows.Forms.Button();
+            this.BPatrat2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Box)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,15 +39,48 @@
             // 
             this.Box.Location = new System.Drawing.Point(0, 0);
             this.Box.Name = "Box";
-            this.Box.Size = new System.Drawing.Size(959, 648);
+            this.Box.Size = new System.Drawing.Size(1057, 632);
             this.Box.TabIndex = 0;
             this.Box.TabStop = false;
+            // 
+            // BTriunghi
+            // 
+            this.BTriunghi.Location = new System.Drawing.Point(171, 661);
+            this.BTriunghi.Name = "BTriunghi";
+            this.BTriunghi.Size = new System.Drawing.Size(75, 23);
+            this.BTriunghi.TabIndex = 1;
+            this.BTriunghi.Text = "Triunghiuri";
+            this.BTriunghi.UseVisualStyleBackColor = true;
+            this.BTriunghi.Click += new System.EventHandler(this.BTriunghi_Click);
+            // 
+            // BPatrate
+            // 
+            this.BPatrate.Location = new System.Drawing.Point(283, 661);
+            this.BPatrate.Name = "BPatrate";
+            this.BPatrate.Size = new System.Drawing.Size(75, 23);
+            this.BPatrate.TabIndex = 2;
+            this.BPatrate.Text = "Clepsidre";
+            this.BPatrate.UseVisualStyleBackColor = true;
+            this.BPatrate.Click += new System.EventHandler(this.BPatrate_Click);
+            // 
+            // BPatrat2
+            // 
+            this.BPatrat2.Location = new System.Drawing.Point(396, 661);
+            this.BPatrat2.Name = "BPatrat2";
+            this.BPatrat2.Size = new System.Drawing.Size(75, 23);
+            this.BPatrat2.TabIndex = 3;
+            this.BPatrat2.Text = "Patrate";
+            this.BPatrat2.UseVisualStyleBackColor = true;
+            this.BPatrat2.Click += new System.EventHandler(this.BPatrat2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1007, 644);
+            this.ClientSize = new System.Drawing.Size(1069, 741);
+            this.Controls.Add(this.BPatrat2);
+            this.Controls.Add(this.BPatrate);
+            this.Controls.Add(this.BTriunghi);
             this.Controls.Add(this.Box);
             this.Name = "Form1";
             this.Text = "Laborator2";
@@ -58,6 +94,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox Box;
+        private System.Windows.Forms.Button BTriunghi;
+        private System.Windows.Forms.Button BPatrate;
+        private System.Windows.Forms.Button BPatrat2;
     }
 }
 
--- Laborator2/Laborator2/Form1.cs
+++ Laborator2/Laborator2/Form1.cs
@@ -54,42 +54,167 @@ namespace Laborator2
             bmp = new Bitmap(Box.Width, Box.Height);
             grp = Graphics.FromImage(bmp);
             // grp.DrawLine(new Pen(Color.Black), 30, 40, 50, 60);
+            //fillPatrat();
+            fillTriunghi();
+
+        }
+        void fillClepsidre()
+        {
             Random rnd = new Random();
             Color culoare;
             Pen p;
             provider = new RNGCryptoServiceProvider();
             List<PointF> l = new List<PointF>();
             List<Color> c = new List<Color>();
-            for (int i=0;i<8;i++)
+            List<float> latura = new List<float>();
+            for (int i = 0; i < 8; i++)
+            {
+                PointF[] points = new PointF[4];
+                do
+                {
+                    points[0] = new PointF((float)(rnd.NextDouble() * 1000 % Box.Width), (float)(rnd.NextDouble() * 1000 % Box.Height));
+                   
+                } while (l.Contains(points[0]));
+                l.Add(points[0]);
+                float lungime;
+                do
+                {
+                    lungime =(float)( (rnd.NextDouble()*500) % Math.Min(Box.Width, Box.Height));
+
+                } while (latura.Contains(lungime));
+                latura.Add(lungime);
+                do
+                {
+                    culoare = Color.FromArgb(RandomInt() % 255, RandomInt() % 255, RandomInt() % 255);
+                } while (c.Contains(culoare));
+                p = new Pen(culoare, 5);
+                points[1] = new PointF(points[0].X + lungime, points[0].Y);
+                points[2] = new PointF(points[0].X, points[0].Y + lungime);
+                points[3] = new PointF(points[0].X + lungime, points[0].Y + lungime);
+
+                grp.DrawLine(p, points[0], points[1]);
+
+                grp.DrawLine(p, points[0], points[2]);
+                grp.DrawLine(p, points[2], points[3]);
+                grp.DrawLine(p, points[1], points[3]);
+                grp.FillPolygon(new SolidBrush(p.Color), points);
+                /*
+                grp.FillPolygon(new SolidBrush(p.Color),new PointF[] { points[0], points[1], points[2] });
+                grp.FillPolygon(new SolidBrush(p.Color), new PointF[] { points[1], points[2], points[3] });
+               */
+                
+            }
+
+            Box.Image = bmp;
+        }
+        void fillPatrat()
+        {
+            Random rnd = new Random();
+            Color culoare;
+            Pen p;
+            provider = new RNGCryptoServiceProvider();
+            List<PointF> l = new List<PointF>();
+            List<Color> c = new List<Color>();
+            List<float> latura = new List<float>();
+            for (int i = 0; i < 8; i++)
+            {
+                PointF[] points = new PointF[4];
+                do
+                {
+                    points[0] = new PointF((float)(rnd.NextDouble() * 1000 % Box.Width), (float)(rnd.NextDouble() * 1000 % Box.Height));
+
+                } while (l.Contains(points[0]));
+                l.Add(points[0]);
+                float lungime;
+                do
+                {
+                    lungime = (float)((rnd.NextDouble() * 500) % Math.Min(Box.Width, Box.Height));
+
+                } while (latura.Contains(lungime));
+                latura.Add(lungime);
+                do
+                {
+                    culoare = Color.FromArgb(RandomInt() % 255, RandomInt() % 255, RandomInt() % 255);
+                } while (c.Contains(culoare));
+                p = new Pen(culoare, 5);
+                points[1] = new PointF(points[0].X + lungime, points[0].Y);
+                points[2] = new PointF(points[0].X, points[0].Y + lungime);
+                points[3] = new PointF(points[0].X + lungime, points[0].Y + lungime);
+
+                grp.DrawLine(p, points[0], points[1]);
+
+                grp.DrawLine(p, points[0], points[2]);
+                grp.DrawLine(p, points[2], points[3]);
+                grp.DrawLine(p, points[1], points[3]);
+                grp.FillPolygon(new SolidBrush(p.Color), points);
+               
+                grp.FillPolygon(new SolidBrush(p.Color),new PointF[] { points[0], points[1], points[2] });
+                grp.FillPolygon(new SolidBrush(p.Color), new PointF[] { points[1], points[2], points[3] });
+              
+
+            }
+            Box.Image = bmp;
+        }
+        void fillTriunghi()
+        {
+            Random rnd = new Random();
+            Color culoare;
+            Pen p;
+            provider = new RNGCryptoServiceProvider();
+            List<PointF> l = new List<PointF>();
+            List<Color> c = new List<Color>();
+            for (int i = 0; i < 8; i++)
             {
                 PointF[] points = new PointF[3];
-             
-                do {
+
+                do
+                {
                     PointF[] aux = points;
                     for (int j = 0; j < 3; j++)
                     {
                         do
                         {
-                           points[j] = new PointF((float)(rnd.NextDouble() * 1000%Box.Width), (float)(rnd.NextDouble() * 1000%Box.Height));
-                          //  points[j] = new PointF(Math.Abs(RandomFloat() % Box.Width), Math.Abs(RandomFloat() % Box.Height));
+                            points[j] = new PointF((float)(rnd.NextDouble() * 1000 % Box.Width), (float)(rnd.NextDouble() * 1000 % Box.Height));
+                            //  points[j] = new PointF(Math.Abs(RandomFloat() % Box.Width), Math.Abs(RandomFloat() % Box.Height));
                         } while (l.Contains(points[j]));
-                          
+
                         l.Add(points[j]);
                     }
-                    }while (!Triunghi(points));
-                do {
+                } while (!Triunghi(points));
+                do
+                {
                     culoare = Color.FromArgb(RandomInt() % 255, RandomInt() % 255, RandomInt() % 255);
                 } while (c.Contains(culoare));
-                    p = new Pen(culoare, 5);
+                c.Add(culoare);
+                p = new Pen(culoare, 5);
                 grp.DrawLine(p, points[0], points[1]);
-               
+
                 grp.DrawLine(p, points[1], points[2]);
                 grp.DrawLine(p, points[2], points[0]);
-              grp.FillPolygon(new SolidBrush(p.Color), points);
-                
+                grp.FillPolygon(new SolidBrush(p.Color), points);
+
             }
 
             Box.Image = bmp;
+        }
+
+        private void BTriunghi_Click(object sender, EventArgs e)
+        {
+            grp.Clear(Box.BackColor);
+            fillTriunghi();
+
+        }
+
+        private void BPatrate_Click(object sender, EventArgs e)
+        {
+            grp.Clear(Box.BackColor);
+            fillClepsidre();
+        }
+
+        private void BPatrat2_Click(object sender, EventArgs e)
+        {
+            grp.Clear(Box.BackColor);
+            fillPatrat();
 
         }
     }
