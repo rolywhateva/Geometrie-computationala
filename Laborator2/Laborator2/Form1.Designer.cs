54f4a07e63a12d0333810fa85bb1931427831594
veres roland <rolandveres@yahoo.com>
2019-03-14 09:08:55 +0200

    Triunghi

---
+++ Laborator2/Laborator2/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Laborator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Box
+            // 
+            this.Box.Location = new System.Drawing.Point(0, 0);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(959, 648);
+            this.Box.TabIndex = 0;
+            this.Box.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 644);
+            this.Controls.Add(this.Box);
+            this.Name = "Form1";
+            this.Text = "Laborator2";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Box;
+    }
+}
+
---
+++ Laborator2/Laborator2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+namespace Laborator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics grp;
+        Bitmap bmp;
+        RNGCryptoServiceProvider provider;
+        public float RandomFloat()
+        {
+            var byteArray = new byte[4];
+            provider.GetBytes(byteArray);
+            return  BitConverter.ToSingle(byteArray, 0);
+        }
+        public  int  RandomInt()
+        {
+            var byteArray = new byte[4];
+            provider.GetBytes(byteArray);
+            return Math.Abs(BitConverter.ToInt32(byteArray, 0));
+        }
+        public double Distanta(PointF a, PointF b)
+        {
+            return Math.Sqrt((b.Y - a.Y) * (b.Y - a.Y) + (b.X - a.X) * (b.X - a.X));
+        }
+        public bool Triunghi(PointF[] p)
+        {
+            double AB = Distanta(p[0], p[1]);
+            double BC = Distanta(p[1], p[2]);
+            double AC = Distanta(p[0], p[2]);
+            return AB + AC > AC && AB + AC > BC && BC + AC > AB;
+
+        }
+        /*
+        public bool Accept(List<Point> l )
+        {
+
+        }
+        */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Box.Width, Box.Height);
+            grp = Graphics.FromImage(bmp);
+            // grp.DrawLine(new Pen(Color.Black), 30, 40, 50, 60);
+            Random rnd = new Random();
+            Color culoare;
+            Pen p;
+            provider = new RNGCryptoServiceProvider();
+            List<PointF> l = new List<PointF>();
+            List<Color> c = new List<Color>();
+            for (int i=0;i<8;i++)
+            {
+                PointF[] points = new PointF[3];
+             
+                do {
+                    PointF[] aux = points;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        do
+                        {
+                           points[j] = new PointF((float)(rnd.NextDouble() * 1000%Box.Width), (float)(rnd.NextDouble() * 1000%Box.Height));
+                          //  points[j] = new PointF(Math.Abs(RandomFloat() % Box.Width), Math.Abs(RandomFloat() % Box.Height));
+                        } while (l.Contains(points[j]));
+                          
+                        l.Add(points[j]);
+                    }
+                    }while (!Triunghi(points));
+                do {
+                    culoare = Color.FromArgb(RandomInt() % 255, RandomInt() % 255, RandomInt() % 255);
+                } while (c.Contains(culoare));
+                    p = new Pen(culoare, 5);
+                grp.DrawLine(p, points[0], points[1]);
+               
+                grp.DrawLine(p, points[1], points[2]);
+                grp.DrawLine(p, points[2], points[0]);
+              grp.FillPolygon(new SolidBrush(p.Color), points);
+                
+            }
+
+            Box.Image = bmp;
+
+        }
+    }
+}
