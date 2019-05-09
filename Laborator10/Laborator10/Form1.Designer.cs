c3e0cc0d247fe9ec5c5d77aba5bd28009d5699b7
veres roland <rolandveres@yahoo.com>
2019-05-09 08:52:09 +0300

    Problema cu camere

---
+++ Laborator10/Laborator10/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Laborator10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.butonDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(887, 392);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // butonDraw
+            // 
+            this.butonDraw.Location = new System.Drawing.Point(458, 406);
+            this.butonDraw.Name = "butonDraw";
+            this.butonDraw.Size = new System.Drawing.Size(121, 32);
+            this.butonDraw.TabIndex = 1;
+            this.butonDraw.Text = "Deseneaza";
+            this.butonDraw.UseVisualStyleBackColor = true;
+            this.butonDraw.Click += new System.EventHandler(this.butonDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 450);
+            this.Controls.Add(this.butonDraw);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button butonDraw;
+    }
+}
+
---
+++ Laborator10/Laborator10/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laborator10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics grp;
+        Bitmap bmp;
+        List<Point> points = new List<Point>();
+        Dictionary<Point, string> dic = new Dictionary<Point, String>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            grp = Graphics.FromImage(bmp);
+            grp.TranslateTransform(pictureBox.Width / 2, pictureBox.Height / 2);
+           // grp.DrawLine(new Pen(Color.Black), 30, 40, 50, 60);
+        }
+        public Point  SymetricOx(Point p)
+        {
+            Point M = new Point(p.X, 0);
+            int sY = p.Y * (-1);
+            int sX = p.X;
+            return new Point(sX, sY);
+
+        }
+        public void DrawCircle(Point p)
+        {
+            int radius = 3;
+            Color color = Color.Red;
+            grp.DrawEllipse(new Pen(color), p.X, p.Y, radius, radius);
+            grp.FillEllipse(new SolidBrush(color), p.X, p.Y, radius, radius);
+        }
+        public void Noteaza()
+        {
+            Font font = new Font("Georgia", 10, FontStyle.Regular);
+            for (int i = 0; i < points.Count; i++)
+                grp.DrawString(dic[points[i]], font, new SolidBrush(Color.Black), points[i]);
+        }
+        private void butonDraw_Click(object sender, EventArgs e)
+        {
+            points.Clear(); dic.Clear();
+            points.Add(new Point(40, 40));
+            points.Add(new Point(50, 60));
+            points.Add(new Point(60, 40));
+            points.Add(new Point(70, 40));
+            points.Add(new Point(90, 60));
+            points.Add(new Point(110, 60));
+
+            for (int i = 5; i >=0; i--)
+                points.Add(SymetricOx(points[i]));
+            for (int i = 0; i < points.Count; i++)
+                dic.Add(points[i], "P" + (i + 1));
+
+            /*
+            points.Add(SymetricOx(points[0]));
+            points.Add(SymetricOx(points[1]));
+            points.Add(SymetricOx(points[2]));
+            points.Add(SymetricOx(points[3]));
+            points.Add(SymetricOx(points[4]));
+            points.Add(SymetricOx(points[5]));
+            */
+            Point[] pointArray = points.ToArray();
+            grp.DrawPolygon(new Pen(Color.Black), pointArray);
+
+            //Noteaza();
+                
+            DrawCircle(points[1]);
+            DrawCircle(points[5]);
+            pictureBox.Image = bmp;
+
+
+
+
+        }
+    }
+}
