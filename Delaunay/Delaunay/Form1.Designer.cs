461e83c720228bc59d92d9622ddac545e4433cae
Veres Roland <roli25veres@gmail.com>
2019-05-29 21:46:24 +0300

    29.06.2019 Problema 1 + Problema 2 1/2  pt prez

---
+++ Delaunay/Delaunay/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Delaunay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.labelPuncte = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 374);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(336, 393);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(75, 23);
+            this.buttonDraw.TabIndex = 1;
+            this.buttonDraw.Text = "Deseneaza";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // labelPuncte
+            // 
+            this.labelPuncte.AutoSize = true;
+            this.labelPuncte.Location = new System.Drawing.Point(108, 403);
+            this.labelPuncte.Name = "labelPuncte";
+            this.labelPuncte.Size = new System.Drawing.Size(63, 13);
+            this.labelPuncte.TabIndex = 2;
+            this.labelPuncte.Text = "labelPuncte";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 572);
+            this.Controls.Add(this.labelPuncte);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "DelaunayColorat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Label labelPuncte;
+    }
+}
+
---
+++ Delaunay/Delaunay/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delaunay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics grp;
+        Bitmap bmp;
+        string letters = "ABCDEFG";
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            grp = Graphics.FromImage(bmp);
+            labelPuncte.Text = "";
+        }
+        public void ReDraw()
+        {
+            pictureBox.Image = bmp;
+        }
+        PointF[] points = new PointF[] {
+            new PointF(2.6f,2.4f),
+            new PointF(4f,2f),
+            new PointF(4f,1f),
+            new PointF(2.5f,0.5f),
+            new PointF(1.5f,1f),
+            new PointF(1.5f,2f)
+
+        };
+       
+        
+
+           PointF inSide =  new PointF(2.6f, 1.6f) ;
+        public void  ZoomAll(PointF[] points,float val)
+        {
+            for (int i = 0; i < points.Length; i++)
+                points[i] = Zoom(points[i], val);
+       
+        }
+        public  PointF Zoom(PointF point,float val)
+        {
+            return new PointF(point.X * val, point.Y * val);
+        }
+        int count = 0;
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+
+           
+            grp.Clear(pictureBox.BackColor);
+            labelPuncte.Text = "";
+            if (count <= 0)
+            {
+                ZoomAll(points, 110f);
+                inSide = new PointF(inSide.X * 110, inSide.Y * 110);
+                count++;
+            }
+          
+            
+          
+
+            PointF[] ex1 = new PointF[] {points[5],points[5],points[5],points[1],points[1],points[3] };
+            PointF[] ex2 = new PointF[] { inSide, points[1], points[3],inSide,points[3],inSide};
+            List<PointF[]> trigs = new List<PointF[]>();
+            trigs.Add(new PointF[] { points[0], points[1], points[5] });
+            trigs.Add(new PointF[] { points[5], inSide, points[1] });
+            trigs.Add(new PointF[] { points[5], points[3], inSide });
+            trigs.Add(new PointF[] { points[5], points[4], points[3] });
+            trigs.Add(new PointF[] { points[1], inSide, points[3] });
+            trigs.Add(new PointF[] { points[1], points[2], points[3] });
+         
+            FillTrig(trigs);
+            NoteazaAll("ABCDEFG");
+            grp.DrawString("G", new Font("Verdana", 10, FontStyle.Bold), new SolidBrush(Color.Black), inSide);
+
+            Listeaza();
+            labelPuncte.Text += $"G=({inSide.X},{inSide.Y})" + Environment.NewLine;
+            grp.DrawPolygon(new Pen(Color.Black), points);
+            DrawTrig(ex1, ex2, new Pen(Color.Black));
+            ReDraw();
+         
+            
+        }
+        private void NoteazaAll(string letters)
+        {
+            for (int i = 0; i < points.Length; i++)
+                grp.DrawString(letters[i].ToString(), new Font("Verdana", 10, FontStyle.Bold), new SolidBrush(Color.Black), points[i]);
+
+
+        }
+      
+        private void Listeaza()
+        {
+            for (int i = 0; i < points.Length; i++)
+                labelPuncte.Text += $"{letters[i]}=({points[i].X},{points[i].Y})" + Environment.NewLine;
+
+
+        }
+        private void DrawMuchie(PointF A, PointF B,Pen P)
+        {
+            grp.DrawLine(P, A, B);
+        }
+      
+      
+        private void DrawTrig(PointF[] ex1, PointF[] ex2,Pen p)
+        {
+            for (int i = 0; i < ex1.Length; i++)
+                DrawMuchie(ex1[i], ex2[i], p);
+        }
+        private void FillTrig(List<PointF[]> trigs)
+        {
+            for (int i = 0; i < trigs.Count; i++)
+                grp.FillPolygon(new SolidBrush(RandomColor()), trigs[i]);
+        }
+        private Color RandomColor()
+        {
+            return Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+      
+    }
+ 
+}
